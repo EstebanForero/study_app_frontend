4b4937b242f58f66b1e74b3c6f282ccf8b8f9ae9
Esteban Forero <estebanmff@outlook.com>
2025-01-20 16:30:34 -0500

    Added backend file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 
 function App() {
   return (
-    <>
-    </>
+    <div className="bg-gray-800 w-full h-screen p-4">
+      <h1 className="text-white text-center text-2xl font-bold">Study app</h1>
+    </div>
   )
 }
 
---
+++ src/backend/entities.ts
@@ -0,0 +1,13 @@
+
+export type StudyTopic = {
+  id: number,
+  name: string,
+  description: string | null,
+  creation_date: string
+}
+
+export type StudyTopicInfo = {
+  name: string,
+  description: string | null,
+  creation_date: string
+}
